d669a5ae6e7553fd3ecb55c4271b910e4892eb88
Thomas Moulard <thomas.moulard@gmail.com>
2014-04-30 15:03:36 +0900

    Clean project

--- copyFromMatlabDir.sh
+++ copyFromMatlabDir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp /media/stanislas/Data/virtual_box/shared_data_windows_7/Matlab/Brossette/matlab_posture_generator/Roboptim/GeneratedFiles/* ./src/GeneratedFiles
+rsync /media/stanislas/Data/virtual_box/shared_data_windows_7/Matlab/Brossette/matlab_posture_generator/Roboptim/GeneratedFiles/* ./src/GeneratedFiles
